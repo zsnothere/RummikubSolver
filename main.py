2c31b9994820ab9741d4fd54c03f44310db64fc0
GluEater <86366270+zsnothere@users.noreply.github.com>
2026-06-08 18:22:10 -0400

    Updated render function for screen wrapping.

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def powerset(l):
         o += [s+[i] for s in o]
     return o
 
-def renderBoard(board, width=900, height=600, tileWidth=50, tileHeight=70, margin=10):
+def renderBoard(board, width=900, height=600, tileWidth=25, tileHeight=45, margin=10):
     colors = {
         1: (220, 60, 60),
         2: (60, 120, 220),
@@ -28,9 +28,10 @@ def renderBoard(board, width=900, height=600, tileWidth=50, tileHeight=70, margi
     pygame.display.set_caption("Rummikub Board")
     font = pygame.font.SysFont(None, 28)
     screen.fill((30, 30, 30))
+    start = margin
     y = margin
     for i in board:
-        x = margin
+        x = start
         for ii in i:
             rect = pygame.Rect(x,y,tileWidth,tileHeight)
             pygame.draw.rect(screen, colors[ii[1]], rect, border_radius=6)
@@ -41,6 +42,9 @@ def renderBoard(board, width=900, height=600, tileWidth=50, tileHeight=70, margi
             screen.blit(text, textRect)
             x += tileWidth
         y += tileHeight+15
+        if y > height-tileHeight-margin:
+            y = margin
+            start += 500
         pygame.display.flip()
     while True:
         for event in pygame.event.get():
@@ -66,7 +70,6 @@ class config:
             B = run[0][0] == 0 and i == 1 # joker at the start
             C = i>1 and (run[i-2][0] == run[i][0]-2 and run[i-1][0] == 0) # joker in the middle
             D = run[i][0]==0 # joker at the end (not implemented yet)
-            print(run[i], A, B, C, D, (run[i][1] != run[i-1][1] or run[i][1] != run[(i+1)%len(run)][1]) and run[i][1]!=0)
             if i>0 and b and (A or B or C or D):
                 if ((run[-1][1] not in [run[i][1], 0]) or (run[(i+1)%len(run)][1] not in [run[i][1], 0])) and run[i][1]!=0: b = False
             elif i>0: b = False
@@ -141,13 +144,32 @@ class solver:
         return "draw"
 
 board = [
-    [(3, 1), (4, 1), (5, 1), (0, 0), (7, 1)],
+    [(1, 1), (2, 1), (3, 1), (4, 1)],
+    [(5, 2), (6, 2), (7, 2), (8, 2)],
+    [(2, 3), (3, 3), (4, 3), (5, 3)],
+    [(9, 4), (10, 4), (11, 4), (12, 4), (13, 4)],
+    [(6, 1), (7, 1), (8, 1)],
+    [(1, 2), (2, 2), (3, 2), (4, 2), (5, 2)],
+    [(9, 1), (9, 2), (9, 3), (0, 0)],
+    [(10, 1), (10, 2), (10, 3), (10, 4)],
+    [(11, 1), (11, 2), (11, 3)],
+    [(12, 1), (12, 2), (12, 3), (12, 4)],
     [(7, 2), (7, 3), (7, 4)],
-    [(0, 0), (12, 2), (13, 2)],
-    [(10, 1), (10, 2), (10, 3)],
-    [(9, 1), (10, 1), (0, 0)]
+    [(8, 1), (8, 2), (8, 3)],
+    [(13, 1), (12, 1), (11, 1), (0, 0)][::-1],
+    [(6, 3), (6, 4), (6, 1)],
+    [(3, 4), (4, 4), (5, 4), (6, 4)]
 ]
-hand = [(5,1), (6, 1), (7, 1), (6, 4), (7, 3)]
+hand = [
+    (1, 3),
+    (4, 4),
+    (5, 1),
+    (8, 4),
+    (9, 2),
+    (11, 4),
+    (0, 0),
+    (2, 1)
+]
 
 c = solver(board, hand)
 s = c.solve()
